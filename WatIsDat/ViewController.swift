cca4d10c539e3fac86951eea6e3ca5d8653bae0f
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-05-24 17:12:29 +0700

    Create tap location recognizer with raycast

--- WatIsDat/ViewController.swift
+++ WatIsDat/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var debugText: UITextView!
+    
+    let bubbleDepth : Float = 0.01 // the 'depth' of 3D text
+    var latestPrediction : String = "THIS IS SPARTA" // a variable containing the latest CoreML prediction
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +32,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        /// TAP GESTURE RECOGNIZER
+        let tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(self.handleTap(gestureRecognizer:))
+        )
+        
+        view.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,6 +57,79 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    // MARK: - Interaction Configuration
+    @objc func handleTap(gestureRecognizer : UITapGestureRecognizer) {
+        // Get screen center point
+        let screenCenterPoint : CGPoint = CGPoint(
+            x: self.sceneView.bounds.midX,
+            y: self.sceneView.bounds.midY
+        )
+        
+        // Create a raycast query using the current frame
+        if let raycastQuery: ARRaycastQuery = sceneView.raycastQuery(
+            from: gestureRecognizer.location(in: self.sceneView),
+            allowing: .estimatedPlane,
+            alignment: .any
+        ) {
+            // Perform the raycast and get the results
+            let raycastResults: [ARRaycastResult] = sceneView.session.raycast(raycastQuery)
+            
+            // Get the closest result from the hit
+            if let closestResult = raycastResults.first {
+                // Get Coordinates of HitTest
+                let transform : matrix_float4x4 = closestResult.worldTransform
+                let worldCoord : SCNVector3 = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+                
+                // Create 3D Text
+                let node : SCNNode = createNewBubbleParentNode(latestPrediction)
+                sceneView.scene.rootNode.addChildNode(node)
+                node.position = worldCoord
+            }
+        }
+
+        
+    }
+    
+    func createNewBubbleParentNode(_ text: String) -> SCNNode {
+        // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
+        
+        // TEXT BILLBOARD CONSTRAINT
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        
+        // BUBBLE-TEXT
+        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
+        var font = UIFont(name: "Arial", size: 0.15)
+        bubble.font = font
+        bubble.alignmentMode = "center"
+        bubble.firstMaterial?.diffuse.contents = UIColor.orange
+        bubble.firstMaterial?.specular.contents = UIColor.white
+        bubble.firstMaterial?.isDoubleSided = true
+        // bubble.flatness // setting this too low can cause crashes.
+        bubble.chamferRadius = CGFloat(bubbleDepth)
+        
+        // BUBBLE NODE
+        let (minBound, maxBound) = bubble.boundingBox
+        let bubbleNode = SCNNode(geometry: bubble)
+        // Centre Node - to Centre-Bottom point
+        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
+        // Reduce default text size
+        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        // CENTRE POINT NODE
+        let sphere = SCNSphere(radius: 0.005)
+        sphere.firstMaterial?.diffuse.contents = UIColor.cyan
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        // BUBBLE PARENT NODE
+        let bubbleNodeParent = SCNNode()
+        bubbleNodeParent.addChildNode(bubbleNode)
+        bubbleNodeParent.addChildNode(sphereNode)
+        bubbleNodeParent.constraints = [billboardConstraint]
+        
+        return bubbleNodeParent
     }
 
     // MARK: - ARSCNViewDelegate
